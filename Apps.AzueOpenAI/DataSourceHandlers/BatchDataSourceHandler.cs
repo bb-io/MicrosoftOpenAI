fec5e53b4648fcb1b7f08981b051a557a065a822
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-10-14 14:06:50 +0300

    Added batch actions

---
+++ Apps.AzueOpenAI/DataSourceHandlers/BatchDataSourceHandler.cs
@@ -0,0 +1,25 @@
+﻿using Apps.AzureOpenAI.Api;
+using Apps.AzureOpenAI.Models.Responses.Batch;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using RestSharp;
+
+namespace Apps.AzureOpenAI.DataSourceHandlers;
+
+public class BatchDataSourceHandler(InvocationContext invocationContext)
+    : BaseInvocable(invocationContext), IAsyncDataSourceHandler
+{
+    public async Task<Dictionary<string, string>> GetDataAsync(DataSourceContext context,
+        CancellationToken cancellationToken)
+    {
+        var restClient = new AzureOpenAiRestClient(InvocationContext.AuthenticationCredentialsProviders);
+        var request = new AzureOpenAiRequest("/openai/batches?api-version=2024-08-01-preview", Method.Get, InvocationContext.AuthenticationCredentialsProviders);
+        
+        var batches = await restClient.ExecuteWithErrorHandling<BatchPaginationResponse>(request);
+        var modelsDictionary = batches.Data
+            .Where(model => context.SearchString == null || model.Id.Contains(context.SearchString))
+            .ToDictionary(model => model.Id, model => model.Id);
+        return modelsDictionary;
+    }
+}
---
+++ Apps.AzueOpenAI/Models/Requests/Batch/GetQualityScoreBatchResultRequest.cs
@@ -0,0 +1,9 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.AzureOpenAI.Models.Requests.Batch;
+
+public class GetQualityScoreBatchResultRequest : GetBatchResultRequest
+{
+    [Display("Throw error on any unexpected result")]
+    public bool? ThrowExceptionOnAnyUnexpectedResult { get; set; }
+}
