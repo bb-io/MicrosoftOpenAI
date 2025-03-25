20271e79ec833169bc5064af935079868298a13e
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-03-25 16:09:19 +0200

    test: 🧪 enhance connection validation tests for clarity; added comments for Arrange, Act, Assert phases; improved readability of test cases

--- Tests.AzureOpenaI/ValidatorTests.cs
+++ Tests.AzureOpenaI/ValidatorTests.cs
@@ -10,9 +10,13 @@ public class ConnectionValidatorTests : TestBase
     [TestMethod]
     public async Task ValidateConnection_WithValidCredentials_ReturnsValid()
     {
+        // Arrange
         var validator = new ConnectionValidator();
 
+        // Act
         var result = await validator.ValidateConnection(Creds, CancellationToken.None);
+        
+        // Assert
         Console.WriteLine(result.Message);
         Assert.IsTrue(result.IsValid);
     }
@@ -20,10 +24,16 @@ public class ConnectionValidatorTests : TestBase
     [TestMethod]
     public async Task ValidateConnection_WithInvalidCredentials_ReturnsInvalid()
     {
+        // Arrange
         var validator = new ConnectionValidator();
+        var newCreds = Creds.Select(x => new AuthenticationCredentialsProvider(
+            x.KeyName, 
+            x.Value + "_incorrect"));
 
-        var newCreds = Creds.Select(x => new AuthenticationCredentialsProvider(x.KeyName, x.Value + "_incorrect"));
+        // Act
         var result = await validator.ValidateConnection(newCreds, CancellationToken.None);
+        
+        // Assert
         Console.WriteLine(result.Message);
         Assert.IsFalse(result.IsValid);
     }
