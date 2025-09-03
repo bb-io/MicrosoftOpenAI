af0b377e2a22c949a6d44eeb05a1a782384b3e37
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-09-03 18:16:43 +0300

    fix XLIFF document partial translation

--- Apps.AzureOpenAI/Utils/Xliff/Extensions.cs
+++ Apps.AzureOpenAI/Utils/Xliff/Extensions.cs
@@ -5,9 +5,9 @@ using Blackbird.Xliff.Utils.Models;
 
 namespace Apps.AzureOpenAI.Utils.Xliff;
 
-public static class Extensions
+public static class Extensions 
 {
-    public static Stream ToStream(this XDocument xDoc)
+    public static Stream ToStream(this XDocument xDoc) 
     {
         var stringWriter = new StringWriter();
         xDoc.Save(stringWriter);
@@ -23,37 +23,15 @@ public static class Extensions
         return memoryStream;
     }
 
-    private static List<Blackbird.Xliff.Utils.Models.Tag> GetTags(string src)
+    public static string RemoveExtraNewLines(string originalString) 
     {
-        var parsedTags = new List<Blackbird.Xliff.Utils.Models.Tag>();
-        var tags = Regex.Matches(src, "<(.*?) (.*?)>(.*?)<\\/\\1>");
-        if (tags is null || tags.Count == 0 ) return parsedTags;
-        var count = 0;
-        foreach ( var tag in tags.Select(x => x.Value)) 
-        {
-            count++;
-            var type = Regex.Match(tag, "<(.*?) ").Groups[1].Value;
-            parsedTags.Add(new Blackbird.Xliff.Utils.Models.Tag 
-            {
-                Position = count,
-                Id = Regex.Match(tag.ToLower(), "id=\"(.*?)\"").Groups[1].Value,
-                Value = type == "g" || type == "x" ? "" : tag,
-                Type = type
-            });
-
-        }
-        return parsedTags;
-    }
-
-    public static string RemoveExtraNewLines(string originalString)
-    {
-        if (!string.IsNullOrWhiteSpace(originalString))
+        if (!string.IsNullOrWhiteSpace(originalString)) 
         {
             var to_modify = originalString;
             to_modify = Regex.Replace(to_modify, @"\r\n(\s+)?", "", RegexOptions.Multiline);
             return to_modify;
         }
-        else
+        else 
         {
             return string.Empty;
         }
@@ -63,8 +41,8 @@ public static class Extensions
     {
         string fileContent;
         Encoding encoding;
-        
-        using (StreamReader inFileStream = new StreamReader(fileStream))
+
+        using (StreamReader inFileStream = new StreamReader(fileStream)) 
         {
             encoding = inFileStream.CurrentEncoding;
             fileContent = inFileStream.ReadToEnd();
@@ -72,25 +50,36 @@ public static class Extensions
 
         var tus = Regex.Matches(fileContent, @"<trans-unit [\s\S]+?</trans-unit>").Select(x => x.Value);
         foreach (var tu in tus) 
-        { 
+        {
             var id = Regex.Match(tu, @"trans-unit id=""(.*?)""").Groups[1].Value;
             if (results.ContainsKey(id)) 
             {
-                var newtu = Regex.Replace(tu, "(<target(.*?)>)([\\s\\S]+?)(</target>)", "${1}" + results[id] +"${4}");
+                var newtu = Regex.Replace(tu, "(<target(.*?)>)([\\s\\S]+?)(</target>)", "${1}" + results[id] + "${4}");
                 fileContent = Regex.Replace(fileContent, Regex.Escape(tu), newtu);
 
-            } 
+            }
             else continue;
         }
         return new MemoryStream(encoding.GetBytes(fileContent));
     }
-    public static Dictionary<string, string> CheckTagIssues(List<TranslationUnit> translationUnits, Dictionary<string, string> results)
+
+    public static Dictionary<string, string> CheckTagIssues(List<TranslationUnit> translationUnits, Dictionary<string, string> results) 
     {
         var changesToImplement = new Dictionary<string, string>();
-        foreach (var update in results)
+        foreach (var update in results) 
         {
+            var translationUnit = translationUnits.FirstOrDefault(x => x.Id == update.Key);
+            if (translationUnit == null) 
+            {
+                continue;
+            }
+
             var newTags = GetTags(update.Value);
-            if (AreTagsOk(translationUnits.FirstOrDefault(x => x.Id == update.Key).Tags, newTags)) 
+            if (AreTagsOk(translationUnit.Tags, newTags)) 
+            {
+                changesToImplement.Add(update.Key, update.Value);
+            }
+            else 
             {
                 changesToImplement.Add(update.Key, update.Value);
             }
@@ -99,37 +88,38 @@ public static class Extensions
         return changesToImplement;
     }
 
-    private static bool AreTagsOk(List<Blackbird.Xliff.Utils.Models.Tag> tags, List<Blackbird.Xliff.Utils.Models.Tag> newTags)
+    private static List<Blackbird.Xliff.Utils.Models.Tag> GetTags(string src) 
     {
-        if (tags.Count != newTags.Count)
-        { return false; }
-        foreach (var tag in newTags)
-        {
-            if (tags.Any(x => x.Id == tag.Id && x.Type == tag.Type && x.Value == tag.Value && x.Position == tag.Position))
-            {
-                continue;
-            }
-            else if (tags.Any(x => x.Id == tag.Id && x.Type == tag.Type && x.Value == tag.Value)) 
-            {
-                if (tag.Type == "bpt" || tag.Type == "ept")
-                {
-                    if (tag.Type == "bpt") 
-                    {
+        var parsedTags = new List<Blackbird.Xliff.Utils.Models.Tag>();
+        var count = 0;
 
-                        if (newTags.Any(x => x.Type == "ept" && x.Id == tag.Id && x.Position > tag.Position)) continue;
-                        else return false;
-                    } 
-                    else if (tag.Type == "ept") 
-                    {
-                        if (newTags.Any(x => x.Type == "bpt" && x.Id == tag.Id && x.Position < tag.Position)) continue;
-                        else return false;
-                    }
-                }
-                else continue;
-            }
-            else return false;
+        var tags = Regex.Matches(src, "<(g|x)[^>]*?(?:/>|>.*?</\\1>)");
+
+        foreach (Match match in tags) 
+        {
+            count++;
+            var fullTag = match.Value;
+            var type = Regex.Match(fullTag, @"^<\s*([a-zA-Z0-9]+)").Groups[1].Value;
+            var id = Regex.Match(fullTag, @"id\s*=\s*""([^""]+)""").Groups[1].Value;
+
+            parsedTags.Add(new Blackbird.Xliff.Utils.Models.Tag 
+            {
+                Position = count,
+                Id = id,
+                Value = type == "g" || type == "x" ? "" : fullTag,
+                Type = type
+            });
         }
-        return true;
+
+        return parsedTags;
+    }
+
+    private static bool AreTagsOk(List<Blackbird.Xliff.Utils.Models.Tag> expected, List<Blackbird.Xliff.Utils.Models.Tag> actual) 
+    {
+        var expectedIds = expected.Select(t => t.Id).ToHashSet();
+        var actualIds = actual.Select(t => t.Id).ToHashSet();
+
+        return expectedIds.IsSubsetOf(actualIds);
     }
 }
 
